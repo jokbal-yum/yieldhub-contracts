625e77f92b6c517748edef893345138cce56b55a
mr_jokbal <jokballady@gmail.com>
2022-03-27 13:38:23 -0700

    Initial Telos changes besides Zap

--- contracts/BIFI/interfaces/common/IBar.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.12;
-
-interface IBar {
-  function enter(uint256 _amount) external;
-  function leave(uint256 _share) external;
-}
--- contracts/BIFI/interfaces/ellipsis/IRewardToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.6.0;
-
-interface IRewardToken {
-    function getReward() external;
-    function minter() external view returns (address);
-}
