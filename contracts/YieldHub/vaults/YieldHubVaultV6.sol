35f67b7fe2a3c9e38825365330d20269294f35a4
mr_jokbal <jokballady@gmail.com>
2022-03-30 13:23:51 -0700

    Adding back immutable - removed by mistake

--- contracts/YieldHub/vaults/YieldHubVaultV6.sol
+++ contracts/YieldHub/vaults/YieldHubVaultV6.sol
@@ -29,7 +29,8 @@ contract YieldHubVaultV6 is ERC20, Ownable, ReentrancyGuard {
     // The strategy currently in use by the vault.
     IStrategy public strategy;
     // The minimum time it has to pass before a strat candidate can be approved.
-    uint256 public approvalDelay;
+    uint256 public immutable approvalDelay;
+
 
     event NewStratCandidate(address implementation);
     event UpgradeStrat(address implementation);
